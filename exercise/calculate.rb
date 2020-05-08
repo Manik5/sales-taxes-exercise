cd46eb3e120164ae098f768b8faf1cbbac1ae332
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-08 21:28:01 +0200

    first commit

---
+++ exercise/calculate.rb
@@ -0,0 +1,95 @@
+class Calculate
+	attr_accessor :total_taxes, :total_price, :items
+
+	def initialize(input_items)
+		@items = input_items
+		@sales_tax = 0.10
+		@imported_tax = 0.05
+		@sum_sales_tax = 0.0
+		@sum_imported_tax = 0.0
+		@total_taxes = 0.0
+		@total_price = 0.0
+		@all_tax = 0.15
+		@round_cent = 1 / 0.05
+	end
+
+	def check_type(type)
+		elements = @items.select { |item| item[type] == true}
+		elements
+	end
+
+	def check_tax(applied_tax)
+		apply_tax = ((applied_tax * @round_cent).ceil / @round_cent)
+		 apply_tax
+	end
+
+	def add_tax(price, quantity, tax)
+		tax_to_add = (price * quantity *tax)
+		new_tax = check_tax(tax_to_add)
+		new_tax
+	end
+
+	def sum_total(total, tax)
+		total += tax
+		total = total.round(2)
+		total
+	end
+
+	# sales taxes 10% ----> 0.10
+
+	def total_sales_tax(elements)
+		elements.each do | element |
+			tax_to_add = add_tax(element[:price], element[:quantity], @sales_tax)
+			element[:total] = sum_total(element[:total], tax_to_add)
+			@sum_sales_tax += tax_to_add
+		end
+	end
+
+	def apply_sales_tax
+		elements = check_type(:element)
+		total_sales_tax(elements)
+		@sum_sales_tax
+	end
+
+	# imported taxes 5% ----> 0.05
+	def total_imported_tax(elements)
+		elements.each do | element |
+			tax_to_add = add_tax(element[:price], element[:quantity], @imported_tax)
+			element[:total] = sum_total(element[:total], tax_to_add)
+			@sum_imported_tax += tax_to_add
+		end
+	end
+
+		def apply_imported_tax
+			elements = @items.select { |item| item[:imported] == true}
+			sum_up = total_imported_tax(elements)
+			@sum_imported_tax
+		end
+
+		# total
+
+		def sum_taxes(sale, import)
+			tax_to_add = sale.to_f + import.to_f
+			tax_to_add
+		end
+
+		def sales_tax
+			@total_taxes = sum_taxes(apply_sales_tax, apply_imported_tax)
+			@total_taxes
+		end
+
+		def pick_price
+			default_price = @items.map { |item| item[:price] }.flatten
+			default_price
+		end
+
+		def sum_price
+			total_price = pick_price.inject(:+)
+			total_price
+		end
+
+		def total
+			@total_price = sum_price + sales_tax
+			@total_price
+		end
+end
---
+++ exercise/input.rb
@@ -0,0 +1,70 @@
+class Input
+	attr_accessor :input_file, :items
+
+	# transforming the txt file into an array of hashes
+	def initialize(folder)
+		@input_file = File.open(File.dirname(File.dirname(__FILE__)) + '/inputs/' + folder).to_a
+		 @exceptions = set_exceptions(File.dirname(File.dirname(__FILE__)) + '/exceptions.txt')
+		 @items = []
+	end
+
+	def set_exceptions(folder)
+		exceptions = File.open(folder).to_a
+		exceptions.map! { |item| item.chomp}
+	 	exceptions
+	end
+
+	def parsing
+		@input_file.each do |item|
+			item= item.strip.split(/\s/)
+			check(item)
+		end
+	end
+
+	def check(item)
+		add_item = {
+			quantity: check_quantity(item),
+			name: check_name(item),
+			price: check_price(item),
+			sales: verify_sales(item),
+			imported: verify_imported(item),
+			total: calculate_total(item)
+		}
+		@items << add_item
+		 @items
+	end
+
+	def check_quantity(item)
+		item[0].to_i
+	end
+
+	def check_name(item)
+		last_element = (item.index "at") - 1
+		pick = item[1..last_element].join(" ")
+		pick
+	end
+
+	def check_price(item)
+		first_element = (item.index "at") + 1
+		last_element = item.size
+		pick = item[first_element..last_element]
+		pick[0].to_f
+	end
+
+	def verify_sales(item)
+		crosscheck = item & @exceptions
+		crosscheck = crosscheck.join(" ")
+		crosscheck != "" ? false : true
+	end
+
+	def verify_imported(item)
+		verify_import = item.include? 'imported'
+		verify_import == true ? true : false
+	end
+
+	def calculate_total(item)
+		total = check_quantity(item) * check_price(item)
+		return total
+	end
+end
+
